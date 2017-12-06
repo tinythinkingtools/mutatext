9ce65ecddccc15bbc6c67c77bd3b19f218ef0214
Andrei Blinov <dus.blinov@gmail.com>
2017-12-06 22:34:15 +0000

    make it better

--- config/production.js
+++ config/production.js
@@ -7,7 +7,7 @@ var settings = {
 	rootPath: rootPath,
 	staticPath: staticPath,
 	envPath: __dirname,
-	port: 5000,
+	port: process.env.PORT || 5000,
 	bodyLimit: '100kb',
 	adminEmail: 'dus.blinov@gmail.com',
 	emailFrom: 'noreply@mutatext.co',
